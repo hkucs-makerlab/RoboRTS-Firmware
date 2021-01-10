fbf1dde0a88773dae23c5a000958e9365f878b2a
hkucs-makerlab <makerlab@cs.hku.hk>
2021-01-10 20:32:41 +0800

    add removing existing build folder

--- build-debug.bat
+++ build-debug.bat
@@ -1,2 +1,3 @@
+rm -r -f build
 mkdir build
 cd build && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -D "CMAKE_TOOLCHAIN_FILE=../tools/CMake/GNU-ARM-Toolchain.cmake" ../
--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-mkdir build
+rm -r -f build
+mkdir -p build
 cd build && cmake -G "Unix Makefiles" -D "CMAKE_TOOLCHAIN_FILE=../tools/CMake/GNU-ARM-Toolchain.cmake" ../
